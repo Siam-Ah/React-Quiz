cb800527c0c589016c9198e915a9f68a4c6c7f34
Siam <ahmedsiam037@gmail.com>
2024-12-25 16:47:43 +1100

    app

---
+++ src/components/Question.js
@@ -0,0 +1,14 @@
+import Options from "./Options";
+
+function Question({ question, dispatch, answer }) {
+  return (
+    <div>
+      <h4>
+        {question.question}
+        <Options question={question} dispatch={dispatch} answer={answer} />
+      </h4>
+    </div>
+  );
+}
+
+export default Question;
